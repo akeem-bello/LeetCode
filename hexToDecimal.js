f5b3a29b488be02405d13a6333f100059ddd2a7f
Akeem Bello <belloakeem07@gmail.com>
2023-12-02 22:23:35 -0700

    Convert hex number to decimal number

---
+++ hexToDecimal.js
@@ -0,0 +1,10 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// Answer
+
+function hexToDec(hexString){
+    const decimalNumber = parseInt(hexString, 16);
+  
+    return decimalNumber;
+  }
+
